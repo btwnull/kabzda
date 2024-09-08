9b222911b19d308e1f335cb7cd243344a0eeada4
Ivan Sobenin <dn.sobenin75@gmail.com>
2024-09-08 20:50:08 +0300

    04lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,35 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Accordion } from "./components/accordion/Accordion";
+import { Rating } from "./components/rating/Rating";
 
 function App() {
   return (
     <div className="App">
-      <AppTitle />
-      <Rating />
-      <Accordion />
+      <PageTitle title={"This is APP Component"} />
+      <PageTitle title={"My friends"} />
+      Article 1
+      <Rating value={5} />
+      <Accordion titleValue={"Menu"} collapsed={false} />
+      <Accordion titleValue={"Users"} collapsed={true} />
+      Article 2
+      <Rating value={0} />
+      <Rating value={1} />
+      <Rating value={2} />
+      <Rating value={3} />
+      <Rating value={4} />
+      <Rating value={5} />
     </div>
   );
 }
 
-function Rating() {
-  return (
-    <div>
-      <Star />
-      <Star />
-      <Star />
-      <Star />
-      <Star />
-    </div>
-  );
-}
+type PageTitlePropsType = {
+  title: string;
+};
 
-function AppTitle() {
-  return <>This is APP Component</>;
-}
-
-function Accordion() {
-  return (
-    <div>
-      <h3>Menu</h3>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
-    </div>
-  );
-}
-
-function Star() {
-  return <div>star</div>;
+function PageTitle(props: PageTitlePropsType) {
+  return <h1>{props.title}</h1>;
 }
 
 export default App;
